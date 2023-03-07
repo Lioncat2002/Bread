f6310826284fd995ef99599067b233108f7d6957
Lioncat2002 <sankhayan2002@gmail.com>
2023-03-07 12:13:57 +0530

    feat: added log function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use gtk::{prelude::*, ButtonsType, DialogFlags, MessageDialog, MessageType, Window};
+mod console;
+use std::fs;
+
+use console::Console;
+
 fn main() {
     let platform = v8::new_default_platform(0, false).make_shared();
     v8::V8::initialize_platform(platform);
@@ -14,43 +18,25 @@ fn main() {
         return;
     }
 
-    //This function will be exposed to calling from javascript and will be on global object
-    let first_function = v8::Function::new(
-        scope,
-        |scope: &mut v8::HandleScope,
-         args: v8::FunctionCallbackArguments,
-         mut rv: v8::ReturnValue| {
-            let arg = args.get(0);
-            let arg_string = arg.to_string(scope).unwrap().to_rust_string_lossy(scope);
-            println!("{}", arg_string);
+    //Alert function
+    let alert_fn = v8::Function::new(scope, Console::alert).unwrap().into();
+    let alert_name = v8::String::new(scope, "alert").unwrap().into();
 
-            MessageDialog::new(
-                None::<&Window>,
-                DialogFlags::empty(),
-                MessageType::Info,
-                ButtonsType::Ok,
-                &arg_string,
-            )
-            .run();
-
-            let returned_value_string = v8::Number::new(scope, 0f64).into();
-            rv.set(returned_value_string);
-        },
-    )
-    .unwrap()
-    .into();
-
-    //Name of function which be used in javascript
-    let name = v8::String::new(scope, "alert").unwrap().into();
-
+    //Cosnole.log function
+    let log_fn = v8::Function::new(scope, Console::log).unwrap().into();
+    let log_name = v8::String::new(scope, "print").unwrap().into();
     //Global javascript object
+
     let global = context.global(scope);
 
     //Set my function to global javascript object
-    global.set(scope, name, first_function);
+    global.set(scope, alert_name, alert_fn);
+    global.set(scope, log_name, log_fn);
 
-    let code = v8::String::new(scope, "alert('This message is from javascript')").unwrap();
+    let src = fs::read_to_string("tests/test.js").unwrap();
+
+    let code = v8::String::new(scope, &src).unwrap();
     let script = v8::Script::compile(scope, code, None).unwrap();
-    let result = script.run(scope).unwrap().to_rust_string_lossy(scope);
-    println!("result: {}", result);
+    let return_val = script.run(scope).unwrap().to_rust_string_lossy(scope);
+    println!("return: {}", return_val);
 }
---
+++ tests/test.js
@@ -0,0 +1,8 @@
+class console{
+    static log(ar) {
+        print(ar);
+        return 0;
+    }
+}
+
+console.log('Hello World');
